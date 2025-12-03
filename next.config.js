7b31d43d0be99d8ff38d03a5066f1107a1acf3af
caution98 <caution98@douzone.com>
2025-12-03 14:58:51 +0900

    서버 설정 수정 테스트

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   trailingSlash: true,
-  output: 'export',
   compiler: {
     emotion: true
   },
